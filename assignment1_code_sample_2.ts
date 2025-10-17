be1e4ae687c3feb7d63521c073f707541f36e330
gmooney <gmooney@rrc.ca>
2025-10-17 18:08:06 -0500

    Adding the vulnerabilities

--- assignment1_code_sample_2.ts
+++ assignment1_code_sample_2.ts
@@ -37,6 +37,12 @@ function sendEmail(to: string, subject: string, body: string) {
     });
 }
 
+
+// Cryptographic failures A02
+//The code uses http instead of https when getting the data from the website.
+//The data is not encrypted and could be intercepted by the attackers.
+// OWASP explains that this includes protocols such as HTTP, SMTP, and FTP.
+//You can use https to help secure the data.
 function getData(): Promise<string> {
     return new Promise((resolve, reject) => {
         http.get('http://insecure-api.com/get-data', (res) => {
@@ -47,6 +53,11 @@ function getData(): Promise<string> {
     });
 }
 
+
+//A03 Injection
+//The SQL query is built with string interpolation so user input gets mixed into the SQL.
+//That means an attacker could inject SQL to read, modify, or delete data.
+//Use positive server-side input validation to ensure only expected data types are accepted.
 function saveToDb(data: string) {
     const connection = mysql.createConnection(dbConfig);
     const query = `INSERT INTO mytable (column1, column2) VALUES ('${data}', 'Another Value')`;
@@ -61,10 +72,20 @@ function saveToDb(data: string) {
         connection.end();
     });
 }
-
+//Broken access control A01
+//The code allows anyone to save the data and send the emails.
+//There are no authentication to check to see who is doing these actions.
+//To help prevent as a example do not let someone save to the data base
+//unless they are logged in and a authorized user
 (async () => {
     const userInput = await getUserInput();
     const data = await getData();
+
+
+//Cryptogrpahic failures
+//The code is sending data in plaintext without encryption .
+//If it gets in the wrong hands sensitive data can be exposed.
+//Use TLS/HTTPS for data in transit and encryption at rest for stored data.
     saveToDb(data);
     sendEmail('admin@example.com', 'User Input', userInput);
 })();
